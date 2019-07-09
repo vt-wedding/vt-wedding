44dc5bfd98f7096e02f8832fd6c34854790b546d
vitalii.badion <vitalii.badion@sigma.software>
2019-07-09 23:00:31 +0300

    Add cyrillic font

--- js/main.js
+++ js/main.js
@@ -214,7 +214,7 @@ Table of Contents:
 			var timeinterval = setInterval(updateClock, 1000);
 		}
 		// set your wedding date here
-		var deadline = 'August 18 2019 17:30:00 GMT+0300';
+		var deadline = 'September 15 2019 12:20:00 GMT+0300';
 		if (countdown){
 			initializeClock('timer', deadline);
 		}
